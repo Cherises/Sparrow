e96e8489d9a747bcebb06129b2529861a41b0b14
Ni Chenyang <88241736+Cherises@users.noreply.github.com>
2022-07-22 08:03:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-"""
-为了方便的实现客户端-服务端-客户端的控制过程，我必须重新编写使用socket方法，就目前来说，和多人聊天室的功能差不多，所以我就按照聊天室的布局来
-进行服务端的开发，共三个程序，分别是客户端，服务端，控制端，而我是控制端，服务端就挂载到我的服务器上，通过服务端的中转站来实现我的远程控制
-目的
-handle{
-            device:control/client
-            type:"command/filedown/fileup/sentence"
-            command:"ls/get/put/del/cmd"
-            value:"D:\\log.txt    shutdown -s -t 0"
-            bytesize:"1024"
-}
-"""
 import json
 import socket
 import threading
@@ -48,12 +36,7 @@ def receive_message(address):
     while True:
         try:
             data_message = server_client_socket[address].recv(1024)
-            # 在接收到报头数据文件后，进行json转码，然后判断接下来的操作，是接收文件，还是返回所需参数
-            # 对于多线程中的receive函数，还是先分析一下报头数据，如果是客户端发来的，就转发给服务端，如果是控制端发来的，就
-            # 转发给客户端
             receive_info = json.loads(data_message.decode('utf-8'))
-            print(receive_info)
-            print("-------------receive-----------")
             # 对收到的信息进行解包分析，如果来自控制端，则分别转发到在下的所有客户端
             # 如果是来自客户端，则都转发给服务端
             # 要考虑到不在线的情况
@@ -62,10 +45,6 @@ def receive_message(address):
                 if receive_info['address'] == 'all':
                     #  如果控制端的地址为all则根据地址表中的地址全部发送，否则指定发送
                     for i in server_address:
-                        print("_________ipadd_________")
-                        print(i)
-                        print("controladd="+control_address)
-                        print("--------------------------")
                         if i != control_address:
                             receive_info['address'] = i
                             send_info = json.dumps(receive_info, ensure_ascii=False)
@@ -90,4 +69,4 @@ if __name__ == '__main__':
     while True:
         time.sleep(5)
         print("---------------------------------------------------------")
-        print(server_client_socket)
+        print(server_address)
